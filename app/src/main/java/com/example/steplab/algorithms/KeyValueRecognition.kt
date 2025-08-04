0cacaf1f24b0e6bb97aae730f4f2c8f997ea0a0e
Forla03 <fraforla2003@gmail.com>
2025-08-04 10:20:48 +0200

    Update algorithms to use non real time algorithms

--- app/src/main/java/com/example/steplab/algorithms/KeyValueRecognition.kt
+++ app/src/main/java/com/example/steplab/algorithms/KeyValueRecognition.kt
@@ -103,4 +103,89 @@ class KeyValueRecognition(private val configuration: Configuration) {
             }
         }
     }
+
+    /**
+     * Recognize peaks and valleys using time filtering method (non-real-time logic).
+     */
+    fun recognizeLocalExtremaTimeFiltering(
+        magnitude: BigDecimal,
+        timestamp: Long
+    ): Boolean {
+        if (magnitude <= previousValue) {
+            valleyDetected = false
+            if (!peakDetected || previousValue > peakValue) {
+                configuration.exExMax?.let { exEx ->
+                    val thS = 0.5 * (configuration.exMax?.toDouble() ?: 0.0 - exEx.toDouble())
+                    if ((configuration.lastStepFirstPhaseTime - (configuration.exMax ?: 0)) > thS) {
+                        val thE = 0.3 * (configuration.lastStepFirstPhaseTime - (configuration.exMax ?: 0))
+                        if ((previousTimestamp - configuration.lastStepFirstPhaseTime) < thE) {
+                            // NO OP
+                        } else {
+                            // Update the peak
+                            configuration.previousLocalMax = configuration.lastLocalMaxAccel
+                            configuration.exExMax = configuration.exMax
+                            configuration.exMax = configuration.lastStepFirstPhaseTime
+                            configuration.lastLocalMaxAccel = previousValue
+                            configuration.lastStepFirstPhaseTime = previousTimestamp
+                            peakDetected = true
+
+                            previousValue = magnitude
+                            previousTimestamp = timestamp
+                            return false
+                        }
+                    }
+                }
+
+                peakValue = previousValue
+                configuration.exExMax = configuration.exMax
+                configuration.exMax = configuration.lastStepFirstPhaseTime
+                configuration.lastLocalMaxAccel = previousValue
+                configuration.lastStepFirstPhaseTime = previousTimestamp
+                peakDetected = true
+            }
+        } else if (peakDetected && magnitude >= previousValue) {
+            if (!valleyDetected) {
+                peakValue = BigDecimal.ZERO
+
+                configuration.exExMin?.let { exEx ->
+                    val thS = 0.5 * (configuration.exMin?.toDouble() ?: 0.0 - exEx.toDouble())
+                    if ((configuration.lastStepSecondPhaseTime - (configuration.exMin ?: 0)) > thS) {
+                        val thE = 0.3 * (configuration.lastStepSecondPhaseTime - (configuration.exMin ?: 0))
+                        if ((previousTimestamp - configuration.lastStepSecondPhaseTime) < thE) {
+                            // NO OP
+                        } else {
+                            // Update the valley
+                            configuration.previousLocalMin = configuration.lastLocalMinAccel
+                            configuration.exExMin = configuration.exMin
+                            configuration.exMin = configuration.lastStepSecondPhaseTime
+                            configuration.lastLocalMinAccel = previousValue
+                            configuration.lastStepSecondPhaseTime = previousTimestamp
+
+                            previousValue = magnitude
+                            previousTimestamp = timestamp
+                            return false
+                        }
+                    }
+                }
+
+                configuration.exExMin = configuration.exMin
+                configuration.exMin = configuration.lastStepSecondPhaseTime
+                configuration.lastLocalMinAccel = previousValue
+                configuration.lastStepSecondPhaseTime = previousTimestamp
+                valleyDetected = true
+            }
+        }
+
+        previousValue = magnitude
+        previousTimestamp = timestamp
+
+        return if (valleyDetected) {
+            peakDetected = false
+            valleyDetected = false
+            true
+        } else {
+            false
+        }
+    }
+
 }
